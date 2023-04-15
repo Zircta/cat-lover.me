dcc8f50d0b621c42d3441a64413c203818ef665b
Zircta <126593996+Zircta@users.noreply.github.com>
2023-04-15 14:02:18 -0400

    Add files via upload

---
+++ assets/scripts/enter.js
@@ -0,0 +1,6 @@
+document.getElementById("enter").addEventListener("click", function() {
+    const target = document.getElementById("enter");
+    target.style.opacity = '0';
+    target.addEventListener('transitionend', () => target.remove());
+
+});
