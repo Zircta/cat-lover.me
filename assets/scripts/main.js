5b91d1658e6956a956bb29187e8738c362bcdb7d
Zircta <126593996+Zircta@users.noreply.github.com>
2023-04-16 18:24:18 -0400

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,11 +4,11 @@ images.forEach(image => {
   image.addEventListener('click', () => {
     const src = image.getAttribute('src');
     if (src === 'assets/img/discord.png') {
-      window.location.href = 'https://discord.com/users/636268679767654430';
+      window.location.href = 'https://discord.com/';
     } else if (src === 'assets/img/github.png') {
-      window.location.href = 'https://github.com/Zircta';
+      window.location.href = 'https://github.com/';
     } else if (src === 'assets/img/twitter.png') {
-      window.location.href = 'https://twitter.com/MyNameIsSyz';
+      window.location.href = 'https://twitter.com/';
     }
   });
 });
