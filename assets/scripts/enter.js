e00743d1aba14464ccda629daf834885dffa95a8
Zircta <126593996+Zircta@users.noreply.github.com>
2023-04-16 18:29:22 -0400

    Delete assets directory

--- assets/scripts/enter.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("enter").addEventListener("click", function() {
-    const target = document.getElementById("enter");
-    target.style.opacity = '0';
-    target.addEventListener('transitionend', () => target.remove());
-
-});
